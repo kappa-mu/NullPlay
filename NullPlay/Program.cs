30df74c940daec1e712cf7bdffe815271488cf56
kappa-mu <krishanu.tito@gmail.com>
2022-07-14 19:45:31 +0530

    Add project files.

---
+++ NullPlay/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using NullPlay;
+
+Console.WriteLine("Hello, World!");
+
+Person person = null;
+
+string personName = person?.FirstName;  //No exception here
+
+try
+{
+    personName = person.FirstName;
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+Console.ReadLine();
