1769ee283c7eed981a207108993b26536ab019c2
kappa-mu <krishanu.tito@gmail.com>
2022-07-14 21:42:43 +0530

    Added more tests

--- NullPlay/Program.cs
+++ NullPlay/Program.cs
@@ -1,18 +1,48 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using NullPlay;
 
-Console.WriteLine("Hello, World!");
+TestcNullPropagator();
+TestNullCoalescingOperator();
 
-Person person = null;
-
-string personName = person?.FirstName;  //No exception here
-
-try
+static void TestcNullPropagator()
 {
-    personName = person.FirstName;
+    Person person = null;
+
+    string personName = person?.FirstName;  //No exception here
+
+    try
+    {
+        personName = person.FirstName; //Exception
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
 }
-catch (Exception ex)
+
+static void TestNullCoalescingOperator()
 {
-    Console.WriteLine(ex.Message);
+    string personName = string.Empty;
+
+    Person person1 = null;
+    personName = person1?.FirstName ?? "John";
+    Console.WriteLine(personName); //John
+
+    Person person2 = new Person() { FirstName = null };
+    personName = person2?.FirstName ?? "Bob";
+    Console.WriteLine(personName); //Bob
+
+    Person person3 = new Person() { FirstName = "Jenny" };
+    personName = person3?.FirstName ?? "John";
+    Console.WriteLine(personName); //Jenny
+
+    int? a = null;
+    var b = (a ?? 0);  //0
+    var c = (a ??= 0); //0
+    Console.WriteLine(b);
+    Console.WriteLine(c);
+
 }
+
 Console.ReadLine();
+
